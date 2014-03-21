ef4c396f6aeff75c96c4435078317a93f888112e
Quinn Avila <hkdnow@yahoo.com>
2014-03-20 21:16:59 -0500

    ajax not working

---
+++ tl-blackjack-webapp/public/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#hit_form input').click(function() {
+    $.ajax({
+      type: 'POST',
+      url: '/game/player/hit',
+    }).done(function(msg)) {
+      alert(msg);
+    });
+
+
+    });
+    
+    return false;
+  });
+});
+
